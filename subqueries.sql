10c302b48b069bb74710057d184f13192ed2185e
msmaxiii <maxwellrg@gmail.com>
2022-08-25 01:16:51 -0500

    added subqueries

---
+++ subqueries.sql
@@ -0,0 +1,11 @@
+-- 1.Get all invoices where the unit_price on the invoice_line is greater than $0.99.
+
+-- 2.Get all playlist tracks where the playlist name is Music.
+
+--3. Get all track names for playlist_id 5.
+
+-- 4.Get all tracks where the genre is Comedy.
+.
+-- 5.Get all tracks where the album is Fireball.
+
+-- 6.Get all tracks for the artist Queen ( 2 nested subqueries ).
